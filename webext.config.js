ca3e23e8c4972f4c3de957550579517e8a57c00f
Radik Islamov <vizid1337@gmail.com>
2024-04-25 15:28:19 +0500

    fix bundle assets

--- webext.config.js
+++ webext.config.js
@@ -1,60 +1,47 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const { get } = require('lodash');
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const { merge } = require('webpack-merge')
+const { get } = require('lodash')
+const CopyPlugin = require('copy-webpack-plugin')
+const RemovePlugin = require('remove-files-webpack-plugin')
 
 module.exports = {
   webpack: (config, { src, target, dev, vendor }) => {
-    var target = path.resolve(target.replace('[vendor]', vendor));
+    var target = path.resolve(target.replace('[vendor]', vendor))
+    config.devtool = false
     config.module = merge(config.module ?? {}, {
-      noParse: /\/assets\/js\//,
+      noParse: [/main\.js$/],
       rules: get(config, 'module.rules', []).concat([
         {
           test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-          type: 'asset',
-        },
-      ]),
-    });
+          type: 'asset'
+        }
+      ])
+    })
     config.plugins = get(config, 'plugins', []).concat([
       new CopyPlugin({
         patterns: [
           {
             context: path.resolve(src),
-            from: path.resolve(src, './assets/js/**'),
-            to: target,
-          },
-          {
-            context: path.resolve(src),
-            from: path.resolve(src, 'redirect-rules.json'),
-            to: target,
-          },
-        ],
+            from: './assets/js/main.bundle',
+            to: 'assets/js/main.js'
+          }
+        ]
       }),
-    ]);
-    config.optimization = merge(get(config, 'optimization', {}), {
-      minimize: !dev,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            format: {
-              comments: /^\**!|@preserve|@license|@cc_on/i,
-            },
-            compress: true,
-            mangle: true,
-          },
-          extractComments: true,
-        }),
-      ],
-    });
+      new RemovePlugin({
+        after: {
+          root: `${target}/assets/js`,
+          include: ['main.bundle']
+        }
+      })
+    ])
     config.resolve = merge(get(config, 'config.resolve', {}), {
       alias: {
-        react: 'preact/compat',
+        'react': 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
-        'react/jsx-runtime': 'preact/jsx-runtime',
-      },
-    });
-    return config;
-  },
-};
+        'react/jsx-runtime': 'preact/jsx-runtime'
+      }
+    })
+    return config
+  }
+}
